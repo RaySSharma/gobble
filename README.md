f2777d1fe899e346f7d389fedca2e272cd66d807
rayssharma <ramon.sharma1@gmail.com>
2019-11-08 20:46:10 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,20 +1,14 @@
-# gobble [![NPM version](https://badge.fury.io/js/gobble.svg)](https://npmjs.org/package/gobble) [![Build Status](https://travis-ci.org/RaySSharma/gobble.svg?branch=master)](https://travis-ci.org/RaySSharma/gobble)
+# gobble [![Build Status](https://travis-ci.org/RaySSharma/gobble.svg?branch=master)](https://travis-ci.org/RaySSharma/gobble)
 
 > Turkey-themed image catalog to boost the holiday mood.
 
-## Installation
-
-```sh
-$ npm install --save gobble
-```
-
 ## Usage
 
-```js
-var gobble = require('gobble');
-gobble();
+```python
+import gobble
+gobble.gobble()
 ```
 
 ## License
 
-MIT © [Ray Sharma](rayssharma.com)
+MIT © [Ray Sharma](https://rayssharma.com)
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+VERSION = '0.1.0'
+
+setup(
+    name='gobbler',
+    version=VERSION,
+    py_modules=['gobble'],
+    url='https://github.com/rayssharma/gobble',
+    download_url='https://github.com/rayssharma/gobble/tarball/{}'.format(VERSION),
+    license='MIT',
+    author='Ray Sharma',
+    author_email='ramon.sharma1@gmail.com',
+    description='Turkey-themed image catalog to boost the holiday mood.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=['pillow==5.2.0'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
